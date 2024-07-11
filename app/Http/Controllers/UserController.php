4bdc2d8d4851ee0e8d699154afedc40f0c1e8bd5
Tóth Erik <totherik420@gmail.com>
2024-07-11 13:16:58 +0200

    Profil létrehozása, ahol tudjuk törölni a saját kérdéseinket

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,8 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\User;
+use App\Models\Question;
 class UserController
-{
-    //
+{public function index(Request $request)
+    {
+        $user = User::find($request->userId);
+        $topicName = $user->questions->map(function($question)
+        {
+            return $question->topic->name;
+        })->unique();
+    
+        return view('users.profile', compact('user', 'topicName'));
+    }
 }
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,31 +6,33 @@
 <body>
     <header>
         <nav>
-        @if (Auth::check())
-            <p>Welcome, {{ Auth::user()->username }}</p>
-            
+            @if (Auth::check())
+                <p>Welcome, {{ Auth::user()->username }}</p>
                 <a href="{{ route('index') }}">Kezdőlap</a>
+                <a href="{{ route('questions.create') }}">Tegyél fel egy új kérdést</a> 
+                <a href="{{ route('users.profile', ['userId' => Auth::user()->id]) }}">Profile</a>
+
+                @if (Auth::user()->is_admin)
+                    <a href="{{ route('topics.showCreateForm') }}">Add New Topic</a>
+                @endif
+
                 <a href="javascript:void(0)" onclick="document.getElementById('logout-form').submit();">Kijelentkezés</a>
-            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                @csrf
-            </form>
-            @if (Auth::user()->is_admin)
-                <a href="{{ route('topics.showCreateForm') }}">Add New Topic</a>
-            @endif
-        @else
-            <p>Welcome, stranger</p>
+                
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                    @csrf
+                </form>
+            @else
+                <p>Welcome, stranger</p>
                 <a href="{{ route('index') }}">Kezdőlap</a>
+                <a href="{{ route('questions.create') }}">Tegyél fel egy új kérdést</a> 
                 <a href="{{ route('registration.form') }}">Regisztráció</a>
                 <a href="{{ route('login.form') }}">Bejelentkezés</a>
-        @endif
-                <a href="{{ route('questions.create') }}">Tegyél fel egy új kérdést</a> 
-            </nav>
+            @endif
+        </nav>
     </header>
 
     <main>
         @yield('content')
     </main>
-
- 
 </body>
 </html>
