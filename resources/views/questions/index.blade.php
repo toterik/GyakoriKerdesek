dd493ae2f07e35cdfc406bbf82412382c24da1cb
Tóth Erik <totherik420@gmail.com>
2024-07-15 13:12:16 +0200

    index oldalon random megválaszolatlan kérdések és legnépszerűbb kérdések megjelennek

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -3,6 +3,7 @@
 @section('title', 'Questions in: ' . $topicName)
 
 @section('content')
+    <h1>Questions in: {{$topicName}}</h1>
     @foreach ($questions as $question)
     <h3>
         <a href="{{ route('questions.show', ['topicName' => $topicName, 'questionId' => $question->id]) }}">
